217b102a5eb3f12b273f9eb1dfa9fc78794e670e
Maël Atzori Maltête <atzorimaltete.mael@gmail.com>
2020-12-02 11:56:41 +0100

    Feat: Working on custom continueStory

--- pages/index.js
+++ pages/index.js
@@ -15,122 +15,126 @@ export default function Home() {
     const storyContainer = useRef(null);
     const outerScrollContainer = useRef(null);
 
-    const customContinueStory = () => {
-        if (null !== story && story.canContinue) {
-            console.log("can continu");
-            story.Continue();
-        }
-    }
-
     const continueStory = (firstTime) => {
-
-        var paragraphIndex = 0;
-        var delay = 1000.0;
+        let paragraphIndex = 0;
+        let delay = 1000.0;
+        let customClasses = [];
 
         // Don't over-scroll past new content
-        var previousBottomEdge = firstTime ? 0 : contentBottomEdgeY();
+        let previousBottomEdge = firstTime ? 0 : contentBottomEdgeY();
 
-        // Generate story text - loop through available content
-        while (story.canContinue) {
+        if (story.canContinue) {
+            console.log("Story can continue");
 
-            // Get ink to generate the next paragraph
-            var paragraphText = story.Continue();
-            var tags = story.currentTags;
+            let paragraphText = story.Continue();
+            let tags = story.currentTags;
+            let choices = story.currentChoices;
 
-            // Any special tags included with this line
-            var customClasses = [];
-            for (var i = 0; i < tags.length; i++) {
-                var tag = tags[i];
-
-                // Detect tags of the form "X: Y". Currently used for IMAGE and CLASS but could be
-                // customised to be used for other things too.
-                var splitTag = splitPropertyTag(tag);
-
-                // IMAGE: src
-                if (splitTag && splitTag.property == "IMAGE") {
-                    var imageElement = document.createElement('img');
-                    imageElement.src = splitTag.val;
-                    storyContainer.current.appendChild(imageElement);
-
-                    showAfter(delay, imageElement);
-                    delay += 200.0;
-                }
-
-                // CLASS: className
-                else if (splitTag && splitTag.property == "CLASS") {
-                    customClasses.push(splitTag.val);
-                }
-
-                // CLEAR - removes all existing content.
-                // RESTART - clears everything and restarts the story from the beginning
-                else if (tag == "CLEAR" || tag == "RESTART") {
-                    removeAll("p");
-                    removeAll("img");
-
-                    // Comment out this line if you want to leave the header visible when clearing
-                    setVisible(".header", false);
-
-                    if (tag == "RESTART") {
-                        restart();
-                        return;
-                    }
-                }
-            }
+            console.log("Curr paragraph:", paragraphText);
+            console.log("Curr tags:", tags);
+            console.log("Curr choices:", choices);
 
             // Create paragraph element (initially hidden)
-            var paragraphElement = document.createElement('p');
+            let paragraphElement = document.createElement('p');
             paragraphElement.innerHTML = paragraphText;
             storyContainer.current.appendChild(paragraphElement);
 
             // Add any custom classes derived from ink tags
-            for (var i = 0; i < customClasses.length; i++)
+            for (let i = 0; i < customClasses.length; i++)
                 paragraphElement.classList.add(customClasses[i]);
 
             // Fade in paragraph after a short delay
             showAfter(delay, paragraphElement);
             delay += 200.0;
-        }
 
-        // Create HTML choices from ink choices
-        story.currentChoices.forEach(function (choice) {
+            if (tags.length > 0) {
+                console.log("Tags:", tags);
 
-            // Create paragraph with anchor element
-            var choiceParagraphElement = document.createElement('p');
-            choiceParagraphElement.classList.add("choice");
-            choiceParagraphElement.innerHTML = `<button class="answer"><span>${choice.text}</span></button>`
-            storyContainer.current.appendChild(choiceParagraphElement);
+                // Any special tags included with this line
+                for (let i = 0; i < tags.length; i++) {
+                    let tag = tags[i];
 
-            // Fade choice in after a short delay
-            showAfter(delay, choiceParagraphElement);
-            delay += 200.0;
+                    // Detect tags of the form "X: Y". Currently used for IMAGE and CLASS but could be
+                    // customised to be used for other things too.
+                    let splitTag = splitPropertyTag(tag);
 
-            // Click on choice
-            var choiceAnchorEl = choiceParagraphElement.querySelectorAll("button.answer")[0];
-            choiceAnchorEl.addEventListener("click", function (event) {
+                    // IMAGE: src
+                    if (splitTag && splitTag.property == "IMAGE") {
+                        let imageElement = document.createElement('img');
+                        imageElement.src = splitTag.val;
+                        storyContainer.current.appendChild(imageElement);
 
-                // Don't follow <a> link
-                event.preventDefault();
+                        showAfter(delay, imageElement);
+                        delay += 200.0;
+                    } else if (splitTag && splitTag.property == "CLASS") {
+                        customClasses.push(splitTag.val);
+                    } else if (tag == "CLEAR" || tag == "RESTART") {
+                        removeAll("p");
+                        removeAll("img");
 
-                // Remove all existing choices
-                removeAll("p.choice");
+                        // Comment out this line if you want to leave the header visible when clearing
+                        setVisible(".header", false);
 
-                // Tell the story where to go next
-                story.ChooseChoiceIndex(choice.index);
+                        if (tag == "RESTART") {
+                            restart();
+                            return;
+                        }
+                    }
+                }
+            }
 
-                // Aaand loop
+            if (choices.length > 0) {
+                console.log("Choices:", choices);
+
+                story.currentChoices.forEach(function (choice) {
+
+                    // Create paragraph with anchor element
+                    let choiceParagraphElement = document.createElement('p');
+                    choiceParagraphElement.classList.add("choice");
+                    choiceParagraphElement.innerHTML = `<button class="answer"><span>${choice.text}</span></button>`
+                    storyContainer.current.appendChild(choiceParagraphElement);
+
+                    // Fade choice in after a short delay
+                    showAfter(delay, choiceParagraphElement);
+                    delay += 200.0;
+
+                    // Click on choice
+                    let choiceAnchorEl = choiceParagraphElement.querySelector("button.answer");
+                    choiceAnchorEl.addEventListener("click", function (event) {
+
+                        // Don't follow <a> link
+                        event.preventDefault();
+
+                        // Remove all existing choices
+                        removeAll("p.choice");
+
+                        // Tell the story where to go next
+                        story.ChooseChoiceIndex(choice.index);
+
+                        // Aaand loop
+                        continueStory();
+                    });
+                });
+            }
+
+            // Extend height to fit
+            // We do this manually so that removing elements and creating new ones doesn't
+            // cause the height (and therefore scroll) to jump backwards temporarily.
+            storyContainer.current.style.height = contentBottomEdgeY() + "px";
+
+            if (!firstTime) {
+                scrollDown(previousBottomEdge);
+            }
+
+            setTimeout(() => {
                 continueStory();
-            });
-        });
-
-        // Extend height to fit
-        // We do this manually so that removing elements and creating new ones doesn't
-        // cause the height (and therefore scroll) to jump backwards temporarily.
-        storyContainer.current.style.height = contentBottomEdgeY() + "px";
-
-        if (!firstTime)
-            scrollDown(previousBottomEdge);
+            }, 1000);
+        }
     }
 
+
+    // Utility functions //
+    
     const restart = () => {
         story.ResetState();
 
@@ -149,24 +153,24 @@ export default function Home() {
 
     // Scrolls the page down, but no further than the bottom edge of what you could
     // see previously, so it doesn't go too far.
-    function scrollDown(previousBottomEdge) {
+    const scrollDown = (previousBottomEdge) => {
 
         // Line up top of screen with the bottom of where the previous content ended
-        var target = previousBottomEdge;
+        let target = previousBottomEdge;
 
         // Can't go further than the very bottom of the page
-        var limit = outerScrollContainer.current.scrollHeight - outerScrollContainer.current.clientHeight;
+        let limit = outerScrollContainer.current.scrollHeight - outerScrollContainer.current.clientHeight;
         if (target > limit) target = limit;
 
-        var start = outerScrollContainer.current.scrollTop;
+        let start = outerScrollContainer.current.scrollTop;
 
-        var dist = target - start;
-        var duration = 300 + 300 * dist / 100;
-        var startTime = null;
+        let dist = target - start;
+        let duration = 300 + 300 * dist / 100;
+        let startTime = null;
         function step(time) {
             if (startTime == null) startTime = time;
-            var t = (time - startTime) / duration;
-            var lerp = 3 * t * t - 2 * t * t * t; // ease in/out
+            let t = (time - startTime) / duration;
+            let lerp = 3 * t * t - 2 * t * t * t; // ease in/out
             outerScrollContainer.current.scrollTo(0, (1.0 - lerp) * start + lerp * target);
             if (t < 1) requestAnimationFrame(step);
         }
@@ -175,26 +179,26 @@ export default function Home() {
 
     // The Y coordinate of the bottom end of all the story content, used
     // for growing the container, and deciding how far to scroll.
-    function contentBottomEdgeY() {
-        var bottomElement = storyContainer.current.lastElementChild;
+    const contentBottomEdgeY = () => {
+        let bottomElement = storyContainer.current.lastElementChild;
         return bottomElement ? bottomElement.offsetTop + bottomElement.offsetHeight : 0;
     }
 
     // Remove all elements that match the given selector. Used for removing choices after
     // you've picked one, as well as for the CLEAR and RESTART tags.
-    function removeAll(selector) {
-        var allElements = storyContainer.current.querySelectorAll(selector);
-        for (var i = 0; i < allElements.length; i++) {
-            var el = allElements[i];
+    const removeAll = (selector) => {
+        let allElements = storyContainer.current.querySelectorAll(selector);
+        for (let i = 0; i < allElements.length; i++) {
+            let el = allElements[i];
             el.parentNode.removeChild(el);
         }
     }
 
     // Used for hiding and showing the header when you CLEAR or RESTART the story respectively.
-    function setVisible(selector, visible) {
-        var allElements = storyContainer.current.querySelectorAll(selector);
-        for (var i = 0; i < allElements.length; i++) {
-            var el = allElements[i];
+    const setVisible = (selector, visible) => {
+        let allElements = storyContainer.current.querySelectorAll(selector);
+        for (let i = 0; i < allElements.length; i++) {
+            let el = allElements[i];
             if (!visible)
                 el.classList.add("invisible");
             else
@@ -205,11 +209,11 @@ export default function Home() {
     // Helper for parsing out tags of the form:
     //  # PROPERTY: value
     // e.g. IMAGE: source path
-    function splitPropertyTag(tag) {
-        var propertySplitIdx = tag.indexOf(":");
+    const splitPropertyTag = (tag) => {
+        let propertySplitIdx = tag.indexOf(":");
         if (propertySplitIdx != null) {
-            var property = tag.substr(0, propertySplitIdx).trim();
-            var val = tag.substr(propertySplitIdx + 1).trim();
+            let property = tag.substr(0, propertySplitIdx).trim();
+            let val = tag.substr(propertySplitIdx + 1).trim();
             return {
                 property: property,
                 val: val
